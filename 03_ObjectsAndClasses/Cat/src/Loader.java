9983dc16a510e7751d0d6ecbbae390b5ffe949d1
Виктор Голованов <vgolovanov10@gmail.com>
2020-10-17 19:27:12 +0300

    7 - objects copying - troubles

--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -9,6 +9,9 @@ public class Loader
         System.out.println(barsik.getDrinkAmount());
         Cat murka = new Cat();
         System.out.println(murka.getWeight());
+        System.out.println();
+        System.out.println(Cat.getCount());
+        System.out.println();
 
         // create our kitten
         Cat murzik = getKitten();
@@ -17,18 +20,42 @@ public class Loader
         System.out.println("Pushistik weight is " + pushistik.getWeight());
         Cat gav = getKitten();
         System.out.println("Gav weight is " + gav.getWeight());
+        System.out.println();
+        gav.feed(123.4);
+        System.out.println("! " + gav.getEatenFood());
+        System.out.println("Gav weight is " + gav.getWeight());
+        System.out.println();
+        System.out.println(Cat.getCount());
+        System.out.println();
 
-        gav.meow();
-        gav.feed(150.501);
-        System.out.println(gav.getWeight());
-        System.out.println(gav.getEatenFood());
-        gav.setName("Gav");
-        System.out.println("Kiten name is " + gav.getName());
-        gav.setColor(Color.BLACK);
-        System.out.println(gav.getColor());
+        // Pafnutiy
+        Cat pafnutiy = getKitten();
+        System.out.println();
+        System.out.println(Cat.getCount());
+        System.out.println();
+        pafnutiy.setName("Пафнутий");
+        pafnutiy.setColor(Color.BLACK_WITH_WHITE);
+        Cat pahom = new Cat(pafnutiy);
+        System.out.println(Cat.getCount());
+        System.out.println();
+        pafnutiy.feed(153.1);
+        System.out.println(pahom.getName());
+        System.out.println(pahom.getColor());
+        System.out.println(pahom.getWeight());
+        pahom.feed(123.4);
+        System.out.println(pahom.getEatenFood());
 
-        barsik.setColor(Color.SANDY);
-        System.out.println(barsik.getColor());
+        // проверяем, останется ли цел Пахом, если что-то станет с Пафнутием
+        while(!pafnutiy.getStatus().equals("Exploded"))
+        {
+            pafnutiy.feed(2345.6);
+        }
+        System.out.println(Cat.getCount());
+        System.out.println();
+        // выясняем, что Пахом тоже оказадся перекормлен, хотя мы его покормили умеренным количеством еды
+        System.out.println(pahom.getStatus());
+        System.out.println(pahom.getEatenFood()); // при этом съел он по-прежнему только 123.4 г
+
     }
     private static Cat getKitten()
     {
