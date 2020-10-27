ee1802a9363a2e51b873e53156dd618103baf116
Виктор Голованов <vgolovanov10@gmail.com>
2020-10-27 12:53:24 +0300

    4.3 GeometryCalculator

--- 04_NumbersStringsAndDates/homework_4.3/GeometryCalculator/src/main/java/GeometryCalculator.java
+++ 04_NumbersStringsAndDates/homework_4.3/GeometryCalculator/src/main/java/GeometryCalculator.java
@@ -1,21 +1,39 @@
 public class GeometryCalculator {
     // метод должен использовать абсолютное значение radius
     public static double getCircleSquare(double radius) {
-        return 0;
+        return Math.PI * Math.pow(Math.abs(radius) , 2);
     }
 
     // метод должен использовать абсолютное значение radius
     public static double getSphereVolume(double radius) {
-        return 0;
+        return (4 * Math.PI * Math.pow(Math.abs(radius) , 3)) / 3;
     }
 
     public static boolean isTrianglePossible(double a, double b, double c) {
-        return false;
+        if(a <= 0 || b <= 0 || c <=0)
+        {
+            return false;
+        }
+        if(a + b > c & a + c > b & b + c > a)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
     // методом isTrianglePossible, если невозможен вернуть -1.0
     public static double getTriangleSquare(double a, double b, double c) {
-        return 0;
+        double triangleSquare = 0.0;
+        double p = ((a + b + c) / 2); // полупериметр
+        if(!isTrianglePossible(a, b, c)){
+            return -1;
+        }
+        else{
+            triangleSquare = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        return triangleSquare;
     }
 }
--- 04_NumbersStringsAndDates/homework_4.3/GeometryCalculator/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.3/GeometryCalculator/src/main/java/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        double squareCircle = GeometryCalculator.getCircleSquare(10.0);
+        System.out.println(squareCircle);
+
+        double volumeSphere = GeometryCalculator.getSphereVolume(27.0);
+        System.out.println(volumeSphere);
+
+        double squareTriangle = GeometryCalculator.getTriangleSquare(12.0, 13.0, 14.0);
+        System.out.println(squareTriangle);
     }
 }
