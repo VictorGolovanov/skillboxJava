69a78a704fa641a10111b9f94209113be0d362ab
Виктор Голованов <vgolovanov10@gmail.com>
2020-10-30 20:00:15 +0300

    4.4 FullNameFormatter

--- 04_NumbersStringsAndDates/homework_4.4/FullNameFormatter/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/FullNameFormatter/src/main/java/Main.java
@@ -12,6 +12,46 @@ public class Main {
       }
       //TODO:напишите ваш код тут, результат вывести в консоль.
       //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+
+      input = input.trim();
+      // посчитаем количество слов используя пробелы
+      // 2 слова - три пробела, если пробелов больше - слов больше
+      // договоримся: если между словами больше 1 пробела - неверный формат ввода
+      // будем использовать подсмотренный цикл for each
+      int spaceCount = 0;
+      for(char space : input.toCharArray())
+      {
+        if(space == ' ')
+        {
+          spaceCount++;
+        }
+      }
+
+      // теперь проверим, есть ли цифры в строке
+      boolean isNumber = false;
+      for(char number : input.toCharArray())
+      {
+        if(Character.isDigit(number))
+        {
+          isNumber = true;
+        }
+      }
+
+      if(isNumber == true || spaceCount != 2)
+      {
+        System.out.println("Введенная строка не является ФИО");
+      }
+      else
+        {
+          String surname = input.substring(0, input.indexOf(" "));
+          String withoutSurname = input.substring(input.indexOf(" ") + 1);
+          String name = withoutSurname.substring(0, withoutSurname.indexOf(" "));
+          String fatherName = withoutSurname.substring(withoutSurname.indexOf(" ") + 1);
+          System.out.println("Фамилия: " + surname);
+          System.out.println("Имя: " + name);
+          System.out.println("Отчество: " + fatherName);
+        }
+
     }
   }
 
