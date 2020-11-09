f60dc8214623804ed4e7b39f53b9d5cf542c87ee
Виктор Голованов <vgolovanov10@gmail.com>
2020-11-09 16:49:02 +0300

    4.5 SplitText

--- 04_NumbersStringsAndDates/homework_4.5/SplitText/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/SplitText/src/main/java/Main.java
@@ -1,12 +1,22 @@
 public class Main {
 
   public static void main(String[] args) {
-
+    System.out.println(splitTextInToWords("qwerty, ... 12?3 qwerty; 456. Abrakadabra 789 djn!"));
   }
 
   public static String splitTextInToWords(String text) {
     //TODO реализуйте метод
-    return "";
+    String clearTextNum = text.replaceAll("\\d+", ""); // сначала цифры
+    String clearTextAll = clearTextNum.replaceAll("[.,;:`!?\\-]", ""); // потом знаки препинания
+    // Заменим все пробелы между словами (сколько бы их ни было) на один пробел между словами
+    String goodString = clearTextAll.replaceAll("\\s+", " ");
+    String[] onlyWords = goodString.split(" ");
+    // а как вывести наш массив через return?
+    /*for(int i = 0; i < onlyWords.length; i++)
+    {
+      System.out.println(onlyWords[i]);
+    }*/
+    return onlyWords;
   }
 
 }
