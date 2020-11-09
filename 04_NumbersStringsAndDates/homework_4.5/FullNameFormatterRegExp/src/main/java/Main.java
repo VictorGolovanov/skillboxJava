ad58f1eda59dc638bbf85fc4f0f31694f560758d
Виктор Голованов <vgolovanov10@gmail.com>
2020-11-09 18:09:22 +0300

    4.5 FullNameFormatterRegExp

--- 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
@@ -12,6 +12,39 @@ public class Main {
       }
       //TODO:напишите ваш код тут, результат вывести в консоль.
       //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+      input = input.trim();
+
+      // проверим, есть ли цифры в строке
+      boolean isNumber = false;
+      for(char number : input.toCharArray())
+      {
+        if(Character.isDigit(number))
+        {
+          isNumber = true;
+        }
+      }
+
+      // разобьем строку на массив строк
+      String[] fullName = input.split(" ");
+
+      // проверим, сколько слов в получившемся массиве
+      boolean hasEnoughWords = false;
+      if(fullName.length == 3)
+      {
+        hasEnoughWords = true;
+      }
+
+      // теперь результат
+      if(isNumber || !hasEnoughWords)
+      {
+        System.out.println("Введенная строка не является ФИО");
+      }
+      else
+        {
+          System.out.println("Фамилия: " + fullName[0]);
+          System.out.println("Имя: " + fullName[1]);
+          System.out.println("Отчество: " + fullName[2]);
+        }
     }
   }
 
