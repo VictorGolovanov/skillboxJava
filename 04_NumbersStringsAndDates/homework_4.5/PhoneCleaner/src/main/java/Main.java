df538f8e61d2982d86bb13a749ea05b7f5cbd8a5
Виктор Голованов <vgolovanov10@gmail.com>
2020-11-10 22:15:57 +0300

    4.5 PhoneCleaner

--- 04_NumbersStringsAndDates/homework_4.5/PhoneCleaner/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/PhoneCleaner/src/main/java/Main.java
@@ -11,6 +11,62 @@ public class Main {
         break;
       }
       //TODO:напишите ваш код тут, результат вывести в консоль.
+
+      // сначала уберем все лишнее
+      String rusPhoneNumber = input.replaceAll("\\D+", "");
+
+      // узнаем, получили ли мы цифры
+      // использовал конструкцию из задания с именами, ибо удобно :)
+      // хотя может это и лишний ход, но так уберегает от ошибок, если при вводе получаем буквы
+      boolean isNumber = false;
+      for(char number : input.toCharArray())
+      {
+        if(Character.isDigit(number))
+        {
+          isNumber = true;
+        }
+      }
+      if(!isNumber){
+        System.out.println("Неверный формат номера");
+      }
+      else{
+
+        // узнаем первую цифру номера и заменим ее, если нужно
+        String result = null;
+        boolean isFirstNumCorrect = false;
+        char firstNum = rusPhoneNumber.charAt(0);
+        if(firstNum == '7')
+        {
+          result = rusPhoneNumber;
+          isFirstNumCorrect = true;
+        }
+        else if(firstNum == '8')
+        {
+          result = "7" + rusPhoneNumber.substring(1);
+          isFirstNumCorrect = true;
+        }
+
+        // узнаем, хватает ли цифр для составления номера
+        boolean hasEnoughNumbers = false;
+        char[] countNumber = rusPhoneNumber.toCharArray();
+        if(countNumber.length == 11)
+        {
+          hasEnoughNumbers = true;
+        }
+        else if(countNumber.length == 10)
+        {
+          result = "7" + rusPhoneNumber;
+          hasEnoughNumbers = true;
+          isFirstNumCorrect = true;
+        }
+        if(hasEnoughNumbers & isFirstNumCorrect)
+        {
+          System.out.println(result);
+        }
+        else{
+          System.out.println("Неверный формат номера");
+        }
+      }
     }
   }
 
