1425abeb4d6240dd81c2cb4c631fcb66b3c12bf9
Виктор Голованов <vgolovanov10@gmail.com>
2020-11-17 13:26:50 +0300

    4.5 MaskingInformationReqExp simpleVersion

--- 04_NumbersStringsAndDates/homework_4.5/MaskingInformationReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/MaskingInformationReqExp/src/main/java/Main.java
@@ -1,12 +1,27 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        String safe = searchAndReplaceDiamonds("Номер кредитной карты <4008> 1234 <5678> 8912", "***");
+        System.out.println(safe);
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
         // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
-        return text;
+        // так проходит все тесты, за исключением более сложного варианта
+        String changedString = text.replaceAll("<.*>", placeholder);
+        /*
+        так не работает
+        Pattern p = Pattern.compile("<.*>");
+        Matcher m = p.matcher(text);
+        while (m.find())
+        {
+            changedString = text.replaceAll("<.*>", placeholder);
+        }
+        */
+        return changedString;
     }
 
 }
