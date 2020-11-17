0f73c25c29386b1c5e37cd235b649432d75aa39a
Виктор Голованов <vgolovanov10@gmail.com>
2020-11-17 13:41:03 +0300

    4.5 MaskingInformationReqExp simplierThenExpected

--- 04_NumbersStringsAndDates/homework_4.5/MaskingInformationReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/MaskingInformationReqExp/src/main/java/Main.java
@@ -11,16 +11,9 @@ public class Main {
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
         // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
         // так проходит все тесты, за исключением более сложного варианта
-        String changedString = text.replaceAll("<.*>", placeholder);
-        /*
-        так не работает
-        Pattern p = Pattern.compile("<.*>");
-        Matcher m = p.matcher(text);
-        while (m.find())
-        {
-            changedString = text.replaceAll("<.*>", placeholder);
-        }
-        */
+        String changedString = text.replaceAll("<(.*?)>", placeholder); // честно говоря, просто поставил скобки и получилось
+        // прокомментируйте пожалуйста результат
+
         return changedString;
     }
 
