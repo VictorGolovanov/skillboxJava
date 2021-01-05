15f2052f9347a7f106d9f9e986d5153e233eab3c
Виктор Голованов <vgolovanov10@gmail.com>
2021-01-05 22:10:01 +0300

    5.4 PhoneBook troubles and questions

--- 05_ArraysAndCollections/homework_5.4/PhoneBook/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.4/PhoneBook/src/main/java/Main.java
@@ -1,5 +1,51 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static PhoneBook phoneBook = new PhoneBook();
+
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        String[] commands = new String[2];
+        commands[0] = "LIST";
+        commands[1] = "STOP";
+
+        while(true)
+        {
+            System.out.println("Введите номер, имя или команду:");
+            String input = scanner.nextLine();
+            if (input.equals(commands[1])) {
+                break;
+            }
+
+            if(input.equals(commands[0]))
+            {
+                // еще не работает
+                phoneBook.getAllContacts();
+            }
+
+            // если сначала введем имя (сырая версия)
+            // еще не написал проверку, есть ли имя в телефонной книге
+            if(phoneBook.isLatin(input) || phoneBook.isRussian(input))
+            {
+                String name = input;
+                // и еще проверить, есть ли уже этот контакт в книге
+                System.out.println("Введите номер телефона для абонента " + name + ":");
+                String phoneInput = scanner.nextLine();
+                if(phoneBook.isContainNumber(phoneInput))
+                {
+                    String phoneNumber = phoneInput;
+                    phoneBook.addContact(phoneNumber, name);
+                }
+                else{
+                    System.out.println("Неверный формат ввода");
+                }
+            }
+            // ...
+            // а потом примерно также, если сначала введем номер телефона
+            // или я иду принципиально не так
+            // и вся основная работа по анализу ввода должна быть реализована в методах PhoneBook.java ?
+        }
     }
 }
--- 05_ArraysAndCollections/homework_5.4/PhoneBook/src/main/java/PhoneBook.java
+++ 05_ArraysAndCollections/homework_5.4/PhoneBook/src/main/java/PhoneBook.java
@@ -1,11 +1,38 @@
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class PhoneBook {
 
+    TreeMap<String, String> phoneBook = new TreeMap<>();
+
     public void addContact(String phone, String name) {
         // проверьте корректность формата имени и телефона
         // если такой номер уже есть в списке, то перезаписать имя абонента
+        boolean isNumber = false;
+        boolean isName = false;
+
+        if(phone.matches("(7{1})(9{1})(\\d{2})(\\d{3})(\\d{4})"))
+        {
+            isNumber = true;
+            System.out.println("ok!");
+        }
+        if(name.matches("[а-яА-ЯёЁ]+"))
+        {
+            isName = true;
+            System.out.println("ok!");
+        }
+        else if(name.matches("[a-zA-Z]+"))
+        {
+            isName = true;
+            System.out.println("ok!");
+        }
+        if(isNumber && isName)
+        {
+            phoneBook.put(name, phone);
+        }
+
+
     }
 
     public String getNameByPhone(String phone) {
@@ -24,6 +51,21 @@ public class PhoneBook {
         // формат одного контакта "Имя - Телефон"
         // если контактов нет в телефонной книге - вернуть пустой TreeSet
         return new TreeSet<>();
+    }
+
+    // для проверки ввода
+    public boolean isContainNumber(String toCheck)
+    {
+        // исходим из формата 79998761234
+        return toCheck.matches("(7{1})(9{1})(\\d{2})(\\d{3})(\\d{4})");
+    }
+
+    public boolean isLatin(String name) {
+        return name.matches("[a-zA-Z]+");
+    }
+
+    public boolean isRussian(String name) {
+        return name.matches("[а-яА-ЯёЁ]+");
     }
 
 }
