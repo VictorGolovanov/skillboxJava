18517b8105bf05fbf7a8c2707545ed0fa128e18b
Виктор Голованов <vgolovanov10@gmail.com>
2021-01-21 17:57:08 +0300

    5.5 CoolNumbers 5 millions

--- 05_ArraysAndCollections/homework_5.5/CoolNumber/src/main/java/CoolNumbers.java
+++ 05_ArraysAndCollections/homework_5.5/CoolNumber/src/main/java/CoolNumbers.java
@@ -1,29 +1,173 @@
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 
 public class CoolNumbers {
 
     public static List<String> generateCoolNumbers() {
-        return Collections.emptyList();
+
+        // добавляем в несколько этапов
+        // так как красота - дело субъективное, то будет так:
+
+        ArrayList<String> carLicensePlates = new ArrayList<>(); // сюда сохраним наши номера
+
+        // условность в том, что не все числа из диапазона являются номерами регионов, но по условию задачи от [1 ; 199]
+        ArrayList<Integer> regionInt = new ArrayList<>();
+        for(int i = 1; i <= 199; i++)
+        {
+            regionInt.add(i);
+        }
+
+        // переделаем в String, чтобы были красивые нули в регионах от 1 до 9
+        ArrayList<String> regionString = new ArrayList<String>();
+        for (Integer integer : regionInt) {
+            if (integer < 10) {
+                regionString.add("0" + integer);
+            } else {
+                regionString.add(Integer.toString(integer));
+            }
+        }
+
+        // массив разрешенных букв для номеров
+        String[] allowedLetters = {"А", "В", "Е", "К", "М", "Н", "О", "Р", "С", "Т", "У", "Х"};
+        int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        // 1) номера с одинаковыми буквами и одинаковыми цифрами: А111АА.region etc.
+        // дает 23880 автомобильных номеров
+        // по регионам: все многообразие сгенерированных номеров умножается на количество регионов
+        for (String reg : regionString) {
+            for (String allowedLetter : allowedLetters) { // по буквам
+                for (int number : numbers) { // по цифрам
+                    String oneCarPlate = String.format("%s%d%d%d%s%s%s", allowedLetter,
+                            number, number, number,
+                            allowedLetter, allowedLetter,
+                            reg);
+                    carLicensePlates.add(oneCarPlate);
+                }
+            }
+        }
+
+        // 2) номера с одинаковыми буквами и не красивыми цифрами цифрами Х793ХХ192.
+        // дает 2 149 200 автомобильных номеров
+        for (String reg : regionString) { // по регионам
+            for (String allowedLetter : allowedLetters) { // по буквам
+                for (int k1 : numbers) { // по цифрам: так как три цифры, то k1 => сотни
+                    for (int k2 : numbers) { // k2 => десятки
+                        for (int k3 : numbers) { // k3 => единицы
+                            String oneCarPlate = String.format("%s%d%d%d%s%s%s", allowedLetter,
+                                    k1, k2, k3,
+                                    allowedLetter, allowedLetter,
+                                    reg);
+                            // хотим исключить номера типа 000 111 222 333, которые уже добавили
+                            if (k1 != k3) // также не попадут номера 010 101 202 и т.д., но мы их можем добавить отдельно
+                            {
+                                carLicensePlates.add(oneCarPlate);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // 3) с не одинаковыми буквами и одинаковыми цифрами
+        // дает 3 152 160 автомобильных номеров
+        for (String reg : regionString){
+            // по буквам. теперь они разные - одинаковые буквы попробуем исключить
+            for(String letter1 : allowedLetters){ // так как и с цифрами - всего три буквы
+                for (String letter2 : allowedLetters){
+                    for (String letter3 : allowedLetters){
+                        for (int number : numbers) {
+                            String oneCarPlate = String.format("%s%d%d%d%s%s%s", letter1,
+                                    number, number, number,
+                                    letter2, letter3,
+                                    reg);
+                            if(!letter1.equals(letter3)) // пропустим ААА ВВВ ... ХХХ и АВА АХА и т.д.
+                            {
+                                carLicensePlates.add(oneCarPlate);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // 4) с одинаковми буквами и особыми цифрами: 101 010 212 121 123 234 ... 987 876 787 878 и т.д.
+        // дает 214 920 автомобильных номеров
+        for (String reg : regionString) { // по регионам
+            for (String allowedLetter : allowedLetters) { // по буквам
+                for (int k1 : numbers) { // по цифрам: так как три цифры, то k1 => сотни
+                    for (int k2 : numbers) { // k2 => десятки
+                        for (int k3 : numbers) { // k3 => единицы
+                            String oneCarPlate = String.format("%s%d%d%d%s%s%s", allowedLetter,
+                                    k1, k2, k3,
+                                    allowedLetter, allowedLetter,
+                                    reg);
+                            // будем добавлять только красивые номера: 101 010 707 070 717 (k1==k3) != k2
+                            if (k1 == k3 && k1 != k2)
+                            {
+                                carLicensePlates.add(oneCarPlate);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return carLicensePlates;
     }
 
     public static boolean bruteForceSearchInList(List<String> list, String number) {
-        return false;
+        boolean isFoundBrute = false;
+        long tBrute = System.nanoTime();
+        if (list.contains(number)) {
+            System.out.println("Поиск перебором: номер найден, поиск занял: " + (System.nanoTime() - tBrute) + " нс.");
+            isFoundBrute = true;
+        } else {
+            System.out.println("Поиск перебором: номер не найден, поиск занял: " + (System.nanoTime() - tBrute) + " нс.");
+        }
+        return isFoundBrute;
     }
 
     public static boolean binarySearchInList(List<String> sortedList, String number) {
-        return false;
+        boolean isFoundBinary = false;
+        long timeBinary = System.nanoTime();
+        if (Collections.binarySearch(sortedList, number) >= 0) {
+            System.out.println("Бинарный поиск: номер найден, поиск занял: " + (System.nanoTime() - timeBinary) + " нс.");
+            isFoundBinary = true;
+        } else {
+            System.out.println("Бинарный поиск: номер не найден, поиск занял: " + (System.nanoTime() - timeBinary) + " нс.");
+        }
+        return isFoundBinary;
     }
 
 
     public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
-        return false;
+        boolean isFoundInHash = false;
+        long timeHash = System.nanoTime();
+        if (hashSet.contains(number)) {
+            System.out.println("Поиск в HashSet: номер найден, поиск занял: " + (System.nanoTime() - timeHash) + "нс");
+            isFoundInHash = true;
+        } else {
+            System.out.println("Поиск в HashSet: номер не найден, поиск занял: " + (System.nanoTime() - timeHash) + "нс");
+        }
+        return isFoundInHash;
     }
 
     public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
-        return false;
+        boolean isFoundInTree = false;
+        long timeTree = System.nanoTime();
+        if (treeSet.contains(number)) {
+            System.out.println("Поиск в TreeSet: номер найден, поиск занял: " + (System.nanoTime() - timeTree) + " нс.");
+            isFoundInTree = true;
+        } else {
+            System.out.println("Поиск в TreeSet: номер не найден, поиск занял: " + (System.nanoTime() - timeTree) + " нс.");
+        }
+        return isFoundInTree;
     }
 
+    public static boolean isCarNumberInString(String curNumber){
+        boolean isNumber = false;
+        String format = "[АВЕКМНОРСТУХ]{1}\\d{3}[АВЕКМНОРСТУХ]{2}\\d{2,}";
+        if(curNumber.matches(format))
+        {
+            isNumber = true;
+        }
+        return isNumber;
+    }
 }
--- 05_ArraysAndCollections/homework_5.5/CoolNumber/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.5/CoolNumber/src/main/java/Main.java
@@ -1,12 +1,46 @@
-public class Main {
-    /*
-    TODO:
-     - реализовать методы класса CoolNumbers
-     - посчитать время поиска введимого номера в консоль в каждой из структуры данных
-     - проанализоровать полученные данные
-     */
+import java.util.*;
 
+public class Main {
+    //public static CoolNumbers coolNumbers = new CoolNumbers();
     public static void main(String[] args) {
+
+
+        System.out.println("Здравствуйте!");
+        System.out.println("Подождите пожалуйста, идут вычисления... :)");
+        List<String> coolNumbersData = CoolNumbers.generateCoolNumbers();
+
+        Collections.sort(coolNumbersData);
+        HashSet<String> hashSet = new HashSet<>(coolNumbersData);
+        TreeSet<String> treeSet = new TreeSet<>(coolNumbersData);
+
+        System.out.println("Количество номеров, сохраненных в базе данных: " + coolNumbersData.size() + ".");
+        while(true)
+        {
+            // для тестов и замера времени поиска
+            /*System.out.println("Первый элемент: " + coolNumbersData.get(0));
+            System.out.println("Средний элемент: " + coolNumbersData.get(2770079));
+            System.out.println("Последний элемент: " + coolNumbersData.get(5540159));*/
+
+            System.out.println("Какой номер вы хотите найти?");
+
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+
+            if (input.equals("0")) {
+                break;
+            }
+
+            if(CoolNumbers.isCarNumberInString(input))
+            {
+                CoolNumbers.bruteForceSearchInList(coolNumbersData, input);
+                CoolNumbers.binarySearchInList(coolNumbersData, input);
+                CoolNumbers.searchInHashSet(hashSet, input);
+                CoolNumbers.searchInTreeSet(treeSet, input);
+            }
+            else{
+                System.out.println("wrong format");
+            }
+        }
 
     }
 }
