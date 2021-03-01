bba004e93ee0591583efb199410d93404c0cd3bf
Виктор Голованов <vgolovanov10@gmail.com>
2021-03-01 17:47:02 +0300

    7.2 Employee

--- 07_AdvancedOOPFeatures/homework_7.2/Employees/src/main/java/Main.java
+++ 07_AdvancedOOPFeatures/homework_7.2/Employees/src/main/java/Main.java
@@ -1,4 +1,7 @@
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 public class Main {
 
@@ -13,6 +16,21 @@ public class Main {
     public static Employee findEmployeeWithHighestSalary(List<Employee> staff, int year) {
         //TODO Метод должен вернуть сотрудника с максимальной зарплатой среди тех,
         // кто пришёл в году, указанном в переменной year
-        return null;
+        Date workYearStart = new Date();
+        workYearStart.setYear(year);
+
+        Optional<Employee> optional = staff
+                .stream()
+                .filter(e -> e.getWorkStart().equals(workYearStart))
+                .max(Comparator.comparing(Employee::getSalary));
+
+        // return optional.orElse(null);
+        if (optional.isPresent())
+        {
+            return optional.get();
+        }
+        else{
+            return null;
+        }
     }
 }
