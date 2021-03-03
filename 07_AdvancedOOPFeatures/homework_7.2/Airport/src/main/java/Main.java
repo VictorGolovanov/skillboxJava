46df2130968242797b33120975dc108158b387ae
Виктор Голованов <vgolovanov10@gmail.com>
2021-03-03 14:50:19 +0300

    7.2 Airport

--- 07_AdvancedOOPFeatures/homework_7.2/Airport/src/main/java/Main.java
+++ 07_AdvancedOOPFeatures/homework_7.2/Airport/src/main/java/Main.java
@@ -1,18 +1,34 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
+import com.skillbox.airport.Terminal;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        Airport airport = Airport.getInstance(); // создаем аэропорт
+        System.out.println("Appropriate flights:");
+        System.out.println(findPlanesLeavingInTheNextTwoHours(airport));
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
         //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+        List<Terminal> terminalList = airport.getTerminals(); // все терминалы аэропорта
+        List<Flight> flightList = new ArrayList<>(); // все полеты
+        for (Terminal terminal : terminalList) { // заполняем рейсами
+            flightList.addAll(terminal.getFlights());
+        }
+
+        Calendar now = Calendar.getInstance();
+        Calendar nextTwoHours = Calendar.getInstance();
+        nextTwoHours.add(Calendar.HOUR, 2);
+
+        return flightList.stream()
+                .filter(f -> f.getDate().after(now.getTime()))
+                .filter(f -> f.getDate().before(nextTwoHours.getTime()))
+                .filter(f -> f.getType().equals(Flight.Type.DEPARTURE))
+                .collect(Collectors.toList());
     }
 
 }
