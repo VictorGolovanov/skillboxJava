28b73684280d8182fac31090ca475365e49136bb
Виктор Голованов <vgolovanov10@gmail.com>
2021-03-28 21:01:35 +0300

    11.1 FolderSize 1

--- 11_FilesAndNetwork/homework_11.1/src/main/java/Main.java
+++ 11_FilesAndNetwork/homework_11.1/src/main/java/Main.java
@@ -1,5 +1,38 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        while(true){
+            System.out.println("Укажите путь до папки:");
+            Scanner scanner = new Scanner(System.in);
+            String path = scanner.nextLine();
+            if(path.equals("STOP")){
+                break;
+            }
+            try{
+                long directorySize = FileUtils.calculateFolderSize(path);
+                System.out.println(convertResult(directorySize));
+            }catch (Exception ex){
+                ex.printStackTrace();
+            }
+        }
     }
+
+    public static String convertResult(long directorySize) {
+        // 1 Kb = 1024 b = 2^10 b; Mb = 2^10 Kb etc...
+        String[] memoryUnits = {"байт.", "Кб.", "Мб.", "Гб.", "Тб."};
+        DecimalFormat decimalFormat = new DecimalFormat("###.####");
+        for (int i = memoryUnits.length - 1; i >= 0; i--) {
+            // идем с последнего элемента
+            double convertedResult = directorySize / Math.pow(2, i * 10);
+            if ((long) convertedResult > 0) { // если результат целочисленного деления больше нуля, то все ок,
+                // если нет, то понижаем разрядность.
+                return "Размер папки составляет " + decimalFormat.format(convertedResult) + " " + memoryUnits[i];
+            }
+        }
+        return "0";
+    }
+
 }
