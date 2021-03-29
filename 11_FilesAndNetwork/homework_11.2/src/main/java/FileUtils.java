4e46c8877fd02b2bae034658d43b1821a6241b1e
Виктор Голованов <vgolovanov10@gmail.com>
2021-03-29 15:18:43 +0300

    11.2 CopyFolder with lib

--- 11_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
+++ 11_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
@@ -1,5 +1,41 @@
+import java.io.File;
+
 public class FileUtils {
+
+    static final String WRONG_PATH_MESSAGE = "Путь не существует или указан неверно";
+    static final String FILE_NOT_DIRECTORY_MESSAGE = "Указан файл, а не папка";
+
     public static void copyFolder(String sourceDirectory, String destinationDirectory) {
         // TODO: write code copy content of sourceDirectory to destinationDirectory
+
+        File sourceDir = new File(sourceDirectory);
+        File destinationDir = new File(destinationDirectory);
+
+        directoryCheck(sourceDir);
+        directoryCheck(destinationDir);
+
+        fileCheck(sourceDir);
+        fileCheck(destinationDir);
+
+        try{
+            org.apache.commons.io.FileUtils.copyDirectory(sourceDir, destinationDir);
+            System.out.println("Копирование прошло успешно!");
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
     }
+
+    private static void directoryCheck(File directory){
+        if(!directory.exists()){
+            throw new IllegalArgumentException(WRONG_PATH_MESSAGE + ": " + directory);
+        }
+    }
+
+    private static void fileCheck(File directory){
+        if(directory.isFile()){
+            throw new IllegalArgumentException(FILE_NOT_DIRECTORY_MESSAGE + ": " + directory);
+        }
+    }
+
 }
--- 11_FilesAndNetwork/homework_11.2/src/main/java/Main.java
+++ 11_FilesAndNetwork/homework_11.2/src/main/java/Main.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        while(true){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Укажите путь к исходной папке:");
+            String sourcePath = scanner.nextLine();
+            System.out.println("Укажите путь к папке клону:");
+            String destinationPath = scanner.nextLine();
+            try{
+                FileUtils.copyFolder(sourcePath, destinationPath);
+            }catch (Exception ex){
+                ex.printStackTrace();
+            }
+            if(sourcePath.equals("STOP") || destinationPath.equals("STOP")){
+                break;
+            }
+        }
     }
 }
