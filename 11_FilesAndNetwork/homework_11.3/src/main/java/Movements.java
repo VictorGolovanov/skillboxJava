86903d0b9a9895dad29f79db095b7946c7343947
Виктор Голованов <vgolovanov10@gmail.com>
2021-04-07 19:32:35 +0300

    11.3 MovementList with streams

--- 11_FilesAndNetwork/homework_11.3/src/main/java/Movements.java
+++ 11_FilesAndNetwork/homework_11.3/src/main/java/Movements.java
@@ -3,11 +3,11 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Movements {
     private List<String> lines;
     private List<Transaction> movements;
-    private Set<String> descriptionSet;
 
     public Movements(String pathMovementsCsv){
         loadMovementsFromFile(pathMovementsCsv);
@@ -16,7 +16,6 @@ public class Movements {
     private void loadMovementsFromFile(String pathMovementsCsv) {
         try {
             movements = new ArrayList<>();
-            descriptionSet = new TreeSet<>();
             lines = Files.readAllLines(Paths.get(pathMovementsCsv));
             // пропускаем шапку
             for(int i = 1; i < lines.size(); i++){
@@ -48,16 +47,13 @@ public class Movements {
                         String currency = fragments[2];
                         Date dateOfTransaction = new SimpleDateFormat("dd.MM.yyyy").parse(fragments[3]);
                         String referenceOfTransaction = fragments[4];
-                        String description = fragments[5];
+                        String description = fragments[5].split("    ")[1].trim(); // отсекаем все лишнее
                         double income = Double.parseDouble(fragments[6]);
                         double expense  = Double.parseDouble(fragments[7]);
 
                         movements.add(new Transaction(type, number, currency,
                                 dateOfTransaction, referenceOfTransaction,
                                 description, income, expense));
-
-                        String[] descriptionFragments = description.split("    "); // делим по 4 пробелам
-                        descriptionSet.add(descriptionFragments[1].trim()); // страшно смотрится без .trim()
                     }
                 }
             } catch (Exception ex) {
@@ -67,30 +63,20 @@ public class Movements {
 
 
     public double getExpenseSum() {
-        double expenseSum = 0.0;
-        for(Transaction movement : movements){
-            expenseSum += movement.getExpense();
-        }
-        return expenseSum;
+        return movements.stream().mapToDouble(Transaction::getExpense).sum();
     }
 
     public double getIncomeSum() {
-        double incomeSum = 0.0;
-        for(Transaction movement : movements){
-            incomeSum += movement.getIncome();
-        }
-        return incomeSum;
+        return movements.stream().mapToDouble(Transaction::getIncome).sum();
     }
 
     public void getExpensesByDescription(){
-        for(String ds : descriptionSet){
-            double expenseSum = 0.0;
-            for(Transaction movement : movements){
-                if(movement.getExpense() != 0 && movement.getDescription().contains(ds)){
-                    expenseSum += movement.getExpense();
-                }
-            }
-            System.out.print(expenseSum != 0 ? "ИТОГО расход по категории: " + ds + " -> " + expenseSum + " RUB\n" : "");
+        // ключи - описание операции; значения - размер расходов
+        Map<String, Double> map = movements.stream()
+                .collect(Collectors.groupingBy(Transaction::getDescription, Collectors.summingDouble(Transaction::getExpense)));
+
+        for(Map.Entry<String, Double> m : map.entrySet()){
+            System.out.println("ИТОГО расход по категории: " + m.getKey() + " -> " + m.getValue() + " RUB");
         }
     }
 }
