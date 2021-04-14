213016293c31da5c638ee85876531a29bac71ea9
Виктор Голованов <vgolovanov10@gmail.com>
2021-04-14 17:09:53 +0300

    11.4 via stream api

--- 11_FilesAndNetwork/homework_11.4/src/main/java/ImageSaver.java
+++ 11_FilesAndNetwork/homework_11.4/src/main/java/ImageSaver.java
@@ -1,13 +1,13 @@
 import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ImageSaver {
 
@@ -24,29 +24,29 @@ public class ImageSaver {
         Document doc = Jsoup.connect(websiteUrl).get();
         Elements media = doc.select("[src]");
 
-        for(Element image : media){
-            String link = image.attr("abs:src");
-            if(link.contains(".jpg") || link.contains(".png")){
-                imagesLinks.add(link);
-            }
-        }
+        imagesLinks = media.stream()
+                .map(m -> m.attr("abs:src"))
+                .filter(m -> m.contains(".jpg") || m.contains(".png") || m.contains(".jpeg"))
+                .collect(Collectors.toList());
     }
 
     // скачиваем картинки
-    private void downloadImages(String pathToSave) throws IOException {
-        for(String imageUrl : imagesLinks){
-            URL url = new URL(imageUrl);
-            String name = imageUrl.substring(imageUrl.lastIndexOf("/"));
-            FileUtils.copyURLToFile(url, new File(pathToSave + name));
-        }
+    private void downloadImages(String pathToSave) {
+        imagesLinks.stream()
+                .forEach(i -> { // i -> элемент списка imagesLinks
+                    try {
+                        FileUtils.copyURLToFile(new URL(i), new File(pathToSave + i.substring(i.lastIndexOf("/"))));
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
+                    }
+                });
         System.out.println("Копирование прошло успешно!");
     }
 
     public void printSavedImagesNames(){
         System.out.println("Имена скачанных файлов: ");
-        for(String imageUrl : imagesLinks){
-            String name = imageUrl.substring(imageUrl.lastIndexOf("/"));
-            System.out.println("\t" + name.replaceAll("/", ""));
-        }
+        imagesLinks.stream()
+                .map(i -> i.substring(i.lastIndexOf("/")).replaceAll("/", ""))
+                .forEach(i -> System.out.println("=> " + i));
     }
 }
