6d742f2cacf359600d35cee664c4892cbf0cf7f3
Виктор Голованов <vgolovanov10@gmail.com>
2021-05-09 14:52:12 +0300

    12.3 HibernateEntities done

--- 12_SQLAndHibernate/homework.12.3/DataBaseViaHibernate/src/main/java/Main.java
+++ 12_SQLAndHibernate/homework.12.3/DataBaseViaHibernate/src/main/java/Main.java
@@ -1,24 +1,17 @@
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.mapping.PrimaryKey;
-
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main
 {
     public static void main(String[] args) {
-        List<Course> courseList = new ArrayList<>();
         SessionBuilder builder = new SessionBuilder();
         SessionFactory sessionFactory = builder.getSessionFactory();
 
         Session session = sessionFactory.openSession();
-
         Transaction transaction = session.beginTransaction();
 
-        BigInteger courseCount = (BigInteger) session.createSQLQuery("SELECT COUNT(*) FROM skillbox.courses").uniqueResult();
+
 
         Course course1 = session.get(Course.class, 1);
         System.out.println(course1.getSubscriptions());
@@ -40,9 +33,7 @@ public class Main
         System.out.println(purchase.getPrice());
         System.out.println(purchase.getSubscriptionDate());
 
-        //почему-то NullPointerException
-        // выдает ошибку везде, где в названии есть дефис
-        PurchaseList purchaseFirst = session.get(PurchaseList.class, new PurchaseKey("Амбражевич Порфирий", "Веб-разработчик с 0 до PRO"));
+        PurchaseList purchaseFirst = session.get(PurchaseList.class, new PurchaseKey("Кооскора Вениамин", "Java-разработчик"));
         System.out.println(purchaseFirst.getPrice());
         System.out.println(purchaseFirst.getSubscriptionDate());
 
