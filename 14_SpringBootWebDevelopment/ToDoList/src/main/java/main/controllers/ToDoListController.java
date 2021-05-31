ec5ffc57a30da8e4cafaef296a5da5ba7f643a68
Виктор Голованов <vgolovanov10@gmail.com>
2021-05-31 14:36:58 +0300

    14.2 database connected

--- 14_SpringBootWebDevelopment/ToDoList/src/main/java/main/controllers/ToDoListController.java
+++ 14_SpringBootWebDevelopment/ToDoList/src/main/java/main/controllers/ToDoListController.java
@@ -1,36 +1,60 @@
 package main.controllers;
-import main.ToDoListStorage;
 import main.model.Task;
+import main.model.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ToDoListController
 {
+    @Autowired
+    private TaskRepository repository;
+
     @GetMapping("/tasks/")
     public List<Task> list(){
-        return ToDoListStorage.getToDoList();
+        Iterable<Task> tasksIterable = repository.findAll();
+        ArrayList<Task> tasks = new ArrayList<>();
+        for(Task task : tasksIterable){
+            tasks.add(task);
+        }
+        return tasks;
     }
 
     @PostMapping("/tasks/")
     public int addTask(Task task){
-        return ToDoListStorage.addTask(task);
+        Task newTask = repository.save(task);
+        return newTask.getId();
+    }
+
+    @PostMapping("/tasks/{id}")
+    public ResponseEntity updateTaskById(@PathVariable int id, Task newTask){
+        Optional<Task> taskOptional = repository.findById(id);
+        if(taskOptional.isPresent()){
+            newTask.setId(id);
+            repository.save(newTask);
+            return new ResponseEntity(taskOptional, HttpStatus.OK);
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
 
     @DeleteMapping("/tasks/{id}")
     public void delete(@PathVariable int id){
-        ToDoListStorage.deleteTask(id);
+        repository.deleteById(id);
     }
 
     @GetMapping("/tasks/{id}")
     public ResponseEntity get(@PathVariable int id){
-        Task task = ToDoListStorage.getTask(id);
-        if(task == null){
+        Optional<Task> taskOptional = repository.findById(id);
+        if(taskOptional.isEmpty()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
-        return new ResponseEntity(task, HttpStatus.OK);
+        return new ResponseEntity(taskOptional, HttpStatus.OK);
     }
 }
--- 14_SpringBootWebDevelopment/ToDoList/src/main/java/main/model/Task.java
+++ 14_SpringBootWebDevelopment/ToDoList/src/main/java/main/model/Task.java
@@ -1,11 +1,23 @@
 package main.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Task
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+
     private String responsiblePerson;
+
     private String taskName;
+
     private String deadline;
+
 
     public int getId() {
         return id;
