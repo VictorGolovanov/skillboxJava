4f9fa5bf7497c2b533a699f54052d9ecb0f388a6
Виктор Голованов <vgolovanov10@gmail.com>
2021-06-03 13:37:15 +0300

    14.3 use TaskService

--- 14_SpringBootWebDevelopment/ToDoList/src/main/java/main/model/TaskRepository.java
+++ 14_SpringBootWebDevelopment/ToDoList/src/main/java/main/model/TaskRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends CrudRepository<Task, Integer> {
+public interface TaskRepository extends CrudRepository<TaskEntity, Integer> {
 }
