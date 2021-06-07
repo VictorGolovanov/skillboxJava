952c1736a3a1a89e44f298b018597ecd345733c2
Виктор Голованов <vgolovanov10@gmail.com>
2021-06-07 16:08:00 +0300

    14.4 battle with frontend

--- 14_SpringBootWebDevelopment/ToDoList/src/main/java/main/controller/DefaultController.java
+++ 14_SpringBootWebDevelopment/ToDoList/src/main/java/main/controller/DefaultController.java
@@ -1,15 +1,39 @@
 package main.controller;
 
+import main.model.TaskEntity;
+import main.model.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Date;
 
-@RestController
+@Controller
 public class DefaultController {
 
+    @Autowired
+    TaskRepository taskRepository;
+
+    //@Value("${someParameter.value}")
+    //private Integer someParameter;
+
     @RequestMapping
-    public String index(){
-        return (new Date()).toString() + "\n" + Math.random() * 1000;
+    public String index(Model model){
+        Iterable<TaskEntity> taskEntityIterable = taskRepository.findAll();
+        ArrayList<TaskEntity> tasks = new ArrayList<>();
+        for(TaskEntity task : taskEntityIterable){
+            tasks.add(task);
+        }
+
+        model.addAttribute("tasks", tasks);
+        model.addAttribute("tasksCount", tasks.size());
+        //model.addAttribute("someParameter", someParameter);
+
+
+        return "index";
     }
 }
---
+++ 14_SpringBootWebDevelopment/ToDoList/src/main/resources/static/js/main.js
@@ -0,0 +1,77 @@
+$(function() {
+
+    const appendTask = function(data) {
+        var taskCode = '<a href="#" class="task-link" data-id="' +
+            data.id + '">' + data.taskName + '</a><br>'; // data.taskName
+        $('#task-list')
+            .append('<div>' + taskCode + '</div>');
+    };
+
+    //Loading books on load page
+    //    $.get('/books/', function(response)
+    //    {
+    //        for(i in response) {
+    //            appendBook(response[i]);
+    //        }
+    //    });
+
+    //Show adding task form
+    $('#show-add-task-form').click(function() {
+        $('#task-form').css('display', 'flex');
+    });
+
+    //Closing adding task form
+    $('#task-form').click(function(event) {
+        if (event.target === this) {
+            $(this).css('display', 'none');
+        }
+    });
+
+    //Getting task
+    $(document).on('click', '.task-link', function() {
+        var link = $(this);
+        var taskId = link.data('id');
+        $.ajax({
+            method: "GET",
+            url: '/tasks/' + taskId,
+            success: function(response) {
+                var dedlineCode = '<span>Дедлайн:' + response.deadline + '</span>';
+                link.parent().append(dedlineCode);
+                var personCode = '<span>Ответственное лицо:' + response.responsiblePerson + '</span>';
+                link.parent.append(personCode);
+            },
+            error: function(response) {
+                if (response.status == 404) {
+                    alert('Задача не найдена!');
+                }
+            }
+        });
+        return false;
+    });
+
+    //Adding task
+    $('#save-task').click(function() {
+        var data = $('#task-form form').serialize();
+        $.ajax({
+            method: "POST",
+            url: '/tasks/',
+            data: data,
+            success: function(response) {
+                $('#task-form').css('display', 'none');
+                var task = {};
+                task.id = response;
+                var dataArray = $('#task-form form').serializeArray();
+                for (i in dataArray) {
+                    task[dataArray[i]['name']] = dataArray[i]['value'];
+                }
+                appendTask(task);
+            }
+        });
+        return false;
+    });
+
+    //Updating task
+
+    //Deleting task
+
+});
