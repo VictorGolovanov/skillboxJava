5376008c66e749a0c5660fb445c8bed98e2d7ee5
victor <vgolovanov10@gmail.com>
2021-07-03 10:15:58 +0300

    test commit pc

--- 14_SpringBootWebDevelopment/ToDoList/src/main/java/main/ToDoListApp.java
+++ 14_SpringBootWebDevelopment/ToDoList/src/main/java/main/ToDoListApp.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ToDoListApp {
-    // create database before you run app on new computer or new OS !!!
+
     public static void main(String[] args) {
         SpringApplication.run(ToDoListApp.class, args);
     }
